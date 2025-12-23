f086fa52e8f2014c705e2a3c02102ee77bccf2e1
dennislacara <lacaradennis@gmail.com>
2025-12-23 17:30:33 +0100

    modifica del 23/12/2025

--- database/dao.py
+++ database/dao.py
@@ -19,3 +19,40 @@ class DAO:
         cursor.close()
         conn.close()
         return result
+
+    @staticmethod
+    def get_all_geni():
+        try:
+            conn = DBConnect.get_connection()
+        except Exception as e:
+            print(e)
+
+        lista_cromosomi = set()
+        map_geni = {}
+        cursor = conn.cursor(dictionary=True)
+
+        query = """ SELECT *
+                    FROM gene g 
+                    WHERE g.cromosoma > 0"""
+        cursor.execute(query)
+        for row in cursor:
+            lista_cromosomi.add(row['cromosoma'])
+            map_geni[row['id']] = row['cromosoma']
+        print(lista_cromosomi)
+        print(map_geni)
+
+        cursor.close()
+        conn.close()
+        return lista_cromosomi, map_geni
+
+    @staticmethod
+    def get_all_interazioni():
+        try:
+            conn = DBConnect.get_connection()
+        except Exception as e:
+            print(e)
+
+        cursor = conn.cursor(dictionary=True)
+        query = """ SELECT *
+        FROM interazioni """
+        cursor.execute(query)
--- model/model.py
+++ model/model.py
@@ -1,4 +1,22 @@
 import networkx as nx
+from database.dao import DAO
 
 class Model:
-    pass
+    def __init__(self):
+        self.G = nx.DiGraph()
+
+        self.geni = None
+        self.map_geni = None
+
+        self.interazioni = None
+
+
+        self.load_geni()
+        #self.load_interazioni()
+
+
+    def load_geni(self):
+        self.geni, self.map_geni = DAO.get_all_geni()
+
+    def load_interazioni(self):
+        self.interazioni = DAO.get_all_interazioni()
